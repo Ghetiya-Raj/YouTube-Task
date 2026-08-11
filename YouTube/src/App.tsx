5ec2cafce73f7d2748e0ac1549c0b810dbb21b58
Raj Ghetiya <rajghetiya5@gmail.com>
2026-08-11 16:13:00 +0530

    Fix:Update Project Folder

--- YouTube/src/App.tsx
+++ YouTube/src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-
-import './App.css'
+import RoutesProvider from './routes/RoutesProvider'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
    <div>
-
+    <RoutesProvider/>
 
    </div>
   )
---
+++ YouTube/src/routes/RoutesProvider.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route,Routes } from 'react-router'
+
+const routes = () => {
+  return (
+    <div>routes</div>
+  )
+}
+
+export default routes
